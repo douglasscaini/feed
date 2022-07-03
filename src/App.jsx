a7eba058f9ae106b2f5029284fba1152ccbb5b52
Douglas Scaini <douglasscaini.dev@gmail.com>
2022-07-03 17:15:13 -0300

    Add State

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,8 @@ export function App() {
 
                 <main>
                     {posts.map((post) => (
-                        <Post 
+                        <Post
+                            key={post.id}
                             author={post.author}
                             content={post.content}
                             publishedAt={post.publishedAt}
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -4,7 +4,7 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 
 import styles from './Comment.module.css';
 
-export function Comment() {
+export function Comment({ content }) {
     return (
         <div className={styles.comment}>
             <Avatar
@@ -25,7 +25,7 @@ export function Comment() {
                         </button>
                     </header>
 
-                    <p>Good!</p>
+                    <p>{content}</p>
                 </div>
 
                 <footer>
